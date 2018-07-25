6c80eb99b8ae8b81672fb5d5cd55ab7c34f6dc48
Leila Mardoum <lmardoum@lmardoum-mac2.tsi.lan>
2018-07-25 10:45:04 -0700

    change colors to commands

--- Samples/DataSources/datasources.js
+++ Samples/DataSources/datasources.js
@@ -9,8 +9,8 @@
       var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
       var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
-      var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
-      var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
+      var commands = [ 'reset filters' , 'filter by <filter name> <filter item>' , 'go to <sheet name>', 'show caption', 'view data'];
+      var grammar = '#JSGF V1.0; grammar commands; public <command> = ' + commands.join(' | ') + ' ;'
 
       var recognition = new SpeechRecognition();
       var speechRecognitionList = new SpeechGrammarList();
@@ -25,16 +25,15 @@
       var bg = document.querySelector('html');
       var hints = document.querySelector('.hints');
 
-      var colorHTML= '';
-      colors.forEach(function(v, i, a){
-        colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
+      var commandHTML= '';
+      commands.forEach(function(v, i, a){
+        commandHTML += '<span style="background-command:' + v + ';"> ' + v + ' </span>';
       });
-      hints.innerHTML = 'Tap/click then say a command. Try '+ colorHTML + '.';
+      hints.innerHTML = 'Click, then say a command to affect the dashboard. Try '+ commandHTML + '.';
 
       document.body.onclick = function() {
         recognition.start();
         console.log('Ready to receive a command.');
-        
       } 
 
       recognition.onresult = function(event) {
@@ -51,7 +50,7 @@
         var command = event.results[last][0].transcript;
 
         diagnostic.textContent = 'Result received: ' + command+ '.';
-        bg.style.backgroundColor = command;
+        //bg.style.backgroundcommand = command;
 
         if (command.includes("reset") && command.includes("filter")) {
           resetFilters();
@@ -63,7 +62,7 @@
       }
 
       recognition.onnomatch = function(event) {
-        diagnostic.textContent = "I didn't recognise that color.";
+        diagnostic.textContent = "I didn't recognise that command.";
       }
 
       recognition.onerror = function(event) {
@@ -87,7 +86,7 @@
             var field = filter.fieldName;
             promises.push(worksheet.clearFilterAsync(field));
             
-        });
+          });
         });
       })
       Promise.all(promises).then(function(results){});
